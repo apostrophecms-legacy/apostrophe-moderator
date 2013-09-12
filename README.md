6d4252e67c2b56c03f0809b2231f7a3f90327f69
Kerry Gilbert <kerry@punkave.com>
2013-09-12 17:34:22 -0400

    initial setup. getting this thing to work with apostrophe-site module.

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ apostrophe-moderator
 ====================
 
 This component creates a public-facing interface that allows users to submit instances of existing apostrophe classes to the site's database.
+
+##Usage
+
+In your apostrophe-site configuration, include apostrophe-moderator. Pass it an array of types to be moderated.
+
+```
+  modules: {
+    'apostrophe-blog':      { },
+    'apostrophe-events':    { },
+    'apostrophe-map':       { },
+    'apostrophe-moderator': {
+      // Define the snippets-style modules that will be intercepted by apostrophe-moderator.
+      // Note that I have left out the 'apostrophe-' prefix. This is added automatically.
+      'types': ['events', 'map']
+    }, 
+  },
+```
--- index.js
+++ index.js
@@ -1,3 +1,66 @@
-module.exports = function() {
+var async = require('async');
+var _ = require('underscore');
+var extend = require('extend');
+var snippets = require('apostrophe-snippets');
+var moment = require('moment');
 
+module.exports = moderator;
+
+function moderator(options, callback) {
+  return new moderator.Moderator(options, callback);
+}
+
+moderator.Moderator = function(options, callback) {
+  var self = this;
+  var moderatedTypes = {};
+
+  self.modules = {};
+
+  console.log('Hi. I am a moderator.');
+
+  self.setBridge = function(modules) {
+    self._bridge = modules
+    for(t in options.types) {
+      var type = options.types[t];
+      moderatedTypes[type] = self._bridge['apostrophe-'+type];
+      self.modules = moderatedTypes;
+    }
+  };
+
+  self.intercept = function(callback) {
+    // Loop through the types passed in in the options
+    // and attach moderator 
+    for(t in moderatedTypes) {
+      var type = self.modules[t];
+
+      // attach new functions to the class as such:
+      // type.myNewFunction = function(req, callback) {
+      //   console.log('blah blah blah');
+      //   return callback(null);
+      // }
+
+      // TODO:
+      // - override module's schema to include an approved flag 
+      // - override module.get to exclude items that have 'approved' set to false
+      // - expose routes that the public can post un approved instances of the class to mongo
+      // - include unapproved items in another tab in the 'manage' view of module   
+
+      // Override the dispatch methods like so
+      // (superDispatch is often already defined so we are using
+      // dispatch to cache the original dispatch method)
+      type.ultraDispatch = type.dispatch;
+
+      type.dispatch = function(req, callback) {
+        //put your other stuff in here
+        console.log('HIJACKING YOUR MODULE');
+        return this.ultraDispatch(req, callback);
+      }
+    }
+
+    callback(null);
+ }
+
+  return process.nextTick(function() {
+    return callback(null);
+  });
 }
