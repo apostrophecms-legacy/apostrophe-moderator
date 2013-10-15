110488d0007504d75d10cbd39e5f3b35b4cf04e7
Kerry Gilbert <kerry@punkave.com>
2013-10-15 15:25:43 -0400

    intercepting modules and extending their dispatch methods

--- index.js
+++ index.js
@@ -12,52 +12,139 @@ function moderator(options, callback) {
 
 moderator.Moderator = function(options, callback) {
   var self = this;
-  var moderatedTypes = {};
-
-  self.modules = {};
-
-  console.log('Hi. I am a moderator.');
+  var moderatedTypes = [];
 
   self.setBridge = function(modules) {
-    self._bridge = modules
+    self._bridge = modules;
+
     for(t in options.types) {
       var type = options.types[t];
-      moderatedTypes[type] = self._bridge['apostrophe-'+type];
-      self.modules = moderatedTypes;
+      moderatedTypes.push('apostrophe-'+type);
     }
   };
 
   self.intercept = function(callback) {
+    self._apos = self._bridge[moderatedTypes[0]]._apos;
+    self._apos.pushAsset('template', 'manage', __dirname, '');
+
+    // self._apos.addLocal('aposModeratorMenu', function(args) {
+    //   self._apos.partial('menu', args, _.map(self._bridge, function(module) { return __dirname + '/views'; }
+    // });
+
     // Loop through the types passed in in the options
     // and attach moderator 
-    for(t in moderatedTypes) {
-      var type = self.modules[t];
+    async.each(moderatedTypes, function(_type, callback){
+      var type = self._bridge[_type];
+      self.overrideModule(type, callback);
+    }, function(){
+      callback(null);
+    });
+  }
 
-      // attach new functions to the class as such:
-      // type.myNewFunction = function(req, callback) {
-      //   console.log('blah blah blah');
-      //   return callback(null);
-      // }
-
-      // TODO:
-      // - override module's schema to include an approved flag 
-      // - override module.get to exclude items that have 'approved' set to false
-      // - expose routes that the public can post un approved instances of the class to mongo
-      // - include unapproved items in another tab in the 'manage' view of module   
-
-      // Override the dispatch methods like so
-      // (superDispatch is often already defined so we are using
-      // dispatch to cache the original dispatch method)
-      type.ultraDispatch = type.dispatch;
-
-      type.dispatch = function(req, callback) {
-        //put your other stuff in here
-        console.log('HIJACKING YOUR MODULE');
-        return this.ultraDispatch(req, callback);
-      }
+ self.overrideModule = function(type, callback) {
+  type.ultraDispatch = type.dispatch;
+  type.dispatch = function(req, callback) {
+    if(req.remainder == '/submit') {
+      req.template = type.renderer('submissionForm');
+      return callback(null);
     }
 
-    callback(null);
+    return type.ultraDispatch(req, callback);
+  }
+
+  type.options = {};
+
+  type.superGet = type.get;
+  type.get = function(req, userCriteria, optionsArg, callback) {
+    // adding mandatory criteria prevents anything having to do with the approval queue
+    // from making its way into normal snippet requests
+    var mandatoryCriteria = { approved: { $exists: false }};
+    for(c in userCriteria) { mandatoryCriteria[c] = userCriteria[c] }
+    type.superGet(req, mandatoryCriteria, optionsArg, callback);
+  };
+
+
+  type.addExtraRoutes = function() {
+    type._app.post(type._action +'/submit', function(req, res) {
+      var snippet;
+      var title;
+      var thumbnail;
+      var content;
+      var slug;
+      var published = false;
+
+      title = req.body.title.trim();
+      if (!title.length) {
+        title = type.getDefaultTitle();
+      }
+      slug = type._apos.slugify(title);
+      tags = type._apos.sanitizeTags(req.body.tags);
+
+      snippet = { 
+        title: title, 
+        published: published,
+        type: type._instance,
+        approved: false,
+        tags: tags, 
+        areas: {}, 
+        slug: slug, 
+        createdAt: new Date(), 
+        publishedAt: new Date()
+      };
+
+      snippet.sortTitle = type._apos.sortify(snippet.title);
+      type.convertAllFields('form', req.body, snippet);
+
+      var newSnippet = snippet;
+
+      type.getOne(req, {}, {}, function(err, result){
+        delete result._id;
+        delete result.publishedAt;
+        delete result.published;
+        delete result.searchSummary;
+        delete result.lowSearchText;
+        delete result.highSearchText;
+        delete result.slug;
+
+        for(field in result) {
+          if(req.body[field] && !snippet[field]) {
+            snippet[field] = req.body[field]
+          }
+        }
+
+        async.series([ permissions, beforeInsert, beforeSave, insert, afterInsert, afterSave ], function(){
+          res.send('/')              
+        });
+
+        function permissions(callback) {
+          type._apos.permissions(req, 'edit-' + type._css, null, callback);
+        }
+
+        function beforeInsert(callback) {
+          type.authorAsEditor(req, snippet);
+          return type.beforeInsert(req, req.body, snippet, callback);
+        }
+
+        function beforeSave(callback) {
+          return type.beforeSave(req, req.body, snippet, callback);
+        }
+
+        function insert(callback) {
+          return type.putOne(req, slug, snippet, callback);
+        }
+
+        function afterInsert(callback) {
+          return type.afterInsert(req, req.body, snippet, callback);
+        }
+
+        function afterSave(callback) {
+          return type.afterSave(req, req.body, snippet, callback);
+        }
+      });
+    });
+  }
+  type.addExtraRoutes();
+  callback();
  }
 
   return process.nextTick(function() {
---
+++ public/js/editor.js
@@ -0,0 +1,7 @@
+$(window).load(function() {
+  console.log('fam fam')
+  $('.apos-control.submissions').on('click', function(){
+    alert('fkjhfkjh');
+  });
+});
+
