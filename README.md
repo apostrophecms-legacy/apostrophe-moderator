dd02b0e23df70cd32bebab6db7998a42312f8a79
Tom Boutell <tom@punkave.com>
2013-12-04 15:52:12 -0500

    It works - but I have to address the need for logged out users to participate

--- README.md
+++ README.md
@@ -3,19 +3,138 @@ apostrophe-moderator
 
 This component creates a public-facing interface that allows users to submit instances of existing apostrophe classes to the site's database.
 
-##Usage
+## Code Status
 
-In your apostrophe-site configuration, include apostrophe-moderator. Pass it an array of types to be moderated.
+Currently this module only works for users who are already logged in.
+
+"But I'm trying to receive submissions from the public! That's the whole point!" Yes, we know, so we don't consider this module quite ready for prime time yet.
+
+Our next step is to implement an appropriate level of guest access without the need for an account.
+
+## TODO
+
+* Currently users must be logged in. Address that, without giving guests access to manage other guests' files.
+* The provided filter is effective, but we would prefer to build a classy modal dedicated to moderation which displays a normal view of each article, and consider making the approval process more natural than setting the "published" flag and clicking "save."
+
+## Configuration
+
+In your apostrophe-site configuration, include apostrophe-moderator. Pass it an array of instance types to be moderated. Be sure to specify the permitted fields, otherwise users are presented with all the possible fields which in most cases would include things like `published` that you do not want them to control.
+
+You may use `addFields`, `orderFields` and `removeFields` exactly as you would when extending the schema of any type in app.js. Adding a field might not make sense, but you can use `addFields` to override the definition of a field by name. In order to present fewer widget choices for the `body`, for instance.
+
+The `slug` and `published` fields are always suppressed from user submitted content forms.
+
+See the `apostrophe-schemas` module for more information on the use of these options.
 
 ```
-  modules: {
-    'apostrophe-blog':      { },
-    'apostrophe-events':    { },
-    'apostrophe-map':       { },
-    'apostrophe-moderator': {
-      // Define the snippets-style modules that will be intercepted by apostrophe-moderator.
-      // Note that I have left out the 'apostrophe-' prefix. This is added automatically.
-      'types': ['events', 'map']
-    }, 
-  },
+  types: {
+    'event': {
+      removeFields: [ 'thumbnail' ],
+      addFields: [
+        {
+          name: 'body',
+          label: 'Body',
+          type: 'area',
+          options: {
+            textOnly: true
+          }
+        }
+      ]
+    }
+  }
 ```
+
+Note that eliminating the thumbnail and using the `textOnly` option is only an example. Guests may submit and manage media for use in their own content if you want to let them.
+
+## Adding Submission Buttons To Your Site
+
+<a href="#" data-moderator-submit-button="event">Submit Your Event</a>
+
+Easy, no?
+
+Just make sure the element has the `data-moderator-submit-button` attribute, and the value is the instance type (like "event") to be submitted.
+
+## Adding Submission Forms
+
+For submissions to work you must have a `submissionEditor.html` template in the `views` folder for the appropriate module.
+
+For instance, this could be the content of `lib/modules/events/views/submissionEditor.html`:
+
+```jinja
+{% include 'schemas:schemaMacros.html' %}
+<div class="apos-modal apos-template">
+  <h3>My Event</h3>
+  {% block topButtons %}
+  {% endblock %}
+  <div class="apos-modal-body">
+    <form>
+      {{ schemaFields(fields) }}
+    </form>
+  </div>
+  <div class="apos-modal-footer">
+    <div class="apos-modal-footer-buttons">
+      <a href="#" data-action="dismiss" class="apos-control apos-button apos-cancel">Cancel</a>
+      <a href="#" class="apos-control apos-button apos-save">Submit My Event</a>
+    </div>
+  </div>
+</div>
+```
+
+Note the use of `schemaFields` to render all the fields in the form without a fuss. You can customize the way fields are output; see the [apostrophe-schemas](apostrophe-schemas) module documentation.
+
+## Approving Submitted Content
+
+All submitted content is initially unpublished. In order for admins to effectively moderate incoming content, you will need to override the `manage.html` template of the module in question and add a call to `snippetModerationFilter()`. Be sure to include the appropriate macros file first, like this:
+
+```jinja
+{% include 'snippetMacros.html' %}
+{% include 'moderator:moderatorMacros.html' %}
+
+<div class="apos-modal apos-template {{ manageClass }}">
+  <h3>{% block action %}Manage {{ label | e }}{% endblock %}</h3>
+  <div class="apos-modal-body">
+    <a href="#" class="apos-control apos-button apos-new-button apos-new-snippet-button" {{ newButtonData }}>New {{ instanceLabel | e }}</a>
+    <div class="apos-modal-filters">
+      {{ snippetTrashFilter() }}
+      {{ snippetPublishedFilter() }}
+      {{ snippetDateFilter() }}
+      {{ snippetModerationFilter() }}
+      {{ snippetSearchFilter() }}
+    </div>
+
+    <table data-items class="apos-manage-table">
+      <tr>
+        <th class="apos-manage-events-date">Date</th>
+        <th class="apos-manage-events-title">Title</th>
+        <th class="apos-manage-events-author">Author</th>
+        <th class="apos-manage-events-status">Status</th>
+      </tr>
+      <tr class="apos-template" data-item>
+        <td>
+          <span data-date></span>
+        </td>
+        <td>
+          <span><a href="#" {{ editButtonData }} data-title>Sample Title</a></span>
+        </td>
+        <td></td>
+        <td>
+          <span data-status></span>
+        </td>
+      </tr>
+    </table>
+
+    {# Container for pager, which gets rendered here by js #}
+    <div data-pager-box></div>
+  </div>
+  <div class="apos-modal-footer">
+    <a href="#" data-action="dismiss" class="apos-control apos-button apos-cancel">Done</a>
+  </div>
+</div>
+```
+
+Now admins can select "All" to see all events, or "Pending" to see user-submitted events that are not yet published.
+
+To publish an event an admin simply marks it published.
+
+... And that's it! Your site now supports user-submitted content. Enjoy.
+
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var async = require('async');
 var _ = require('underscore');
 var extend = require('extend');
-var snippets = require('apostrophe-snippets');
 var moment = require('moment');
-
 module.exports = moderator;
 
 function moderator(options, callback) {
@@ -12,142 +10,82 @@ function moderator(options, callback) {
 
 moderator.Moderator = function(options, callback) {
   var self = this;
-  var moderatedTypes = [];
+  self._apos = options.apos;
+  self._app = options.app;
+  self._pages = options.pages;
+  self._action = '/apos-moderator';
+  self._schemas = options.schemas;
 
-  self.setBridge = function(modules) {
-    self._bridge = modules;
+  self._apos.mixinModuleAssets(self, 'moderator', __dirname, options);
 
-    for(t in options.types) {
-      var type = options.types[t];
-      moderatedTypes.push('apostrophe-'+type);
-    }
-  };
+  self.enhance = function(manager, options) {
 
-  self.intercept = function(callback) {
-    self._apos = self._bridge[moderatedTypes[0]]._apos;
-    self._apos.pushAsset('template', 'manage', __dirname, '');
+    // Add a filter that displays as-yet-unpublished user-submitted content
 
-    // self._apos.addLocal('aposModeratorMenu', function(args) {
-    //   self._apos.partial('menu', args, _.map(self._bridge, function(module) { return __dirname + '/views'; }
-    // });
-
-    // Loop through the types passed in in the options
-    // and attach moderator 
-    async.each(moderatedTypes, function(_type, callback){
-      var type = self._bridge[_type];
-      self.overrideModule(type, callback);
-    }, function(){
-      callback(null);
-    });
-  }
-
- self.overrideModule = function(type, callback) {
-  type.ultraDispatch = type.dispatch;
-  type.dispatch = function(req, callback) {
-    if(req.remainder == '/submit') {
-      req.template = type.renderer('submissionForm');
-      return callback(null);
-    }
-
-    return type.ultraDispatch(req, callback);
-  }
-
-  type.options = {};
-
-  type.superGet = type.get;
-  type.get = function(req, userCriteria, optionsArg, callback) {
-    // adding mandatory criteria prevents anything having to do with the approval queue
-    // from making its way into normal snippet requests
-    var mandatoryCriteria = { approved: { $exists: false }};
-    for(c in userCriteria) { mandatoryCriteria[c] = userCriteria[c] }
-    type.superGet(req, mandatoryCriteria, optionsArg, callback);
-  };
-
-
-  type.addExtraRoutes = function() {
-    type._app.post(type._action +'/submit', function(req, res) {
-      var snippet;
-      var title;
-      var thumbnail;
-      var content;
-      var slug;
-      var published = false;
-
-      title = req.body.title.trim();
-      if (!title.length) {
-        title = type.getDefaultTitle();
+    var superGet = manager.get;
+    manager.get = function(req, userCriteria, options, mainCallback) {
+      var filterCriteria = {};
+      if (self._apos.sanitizeBoolean(options.pending)) {
+        // Show unpublished user submissions
+        filterCriteria.published = { $ne: true };
+        filterCriteria.submission = true;
+        userCriteria = { $and: [ userCriteria, filterCriteria ] };
       }
-      slug = type._apos.slugify(title);
-      tags = type._apos.sanitizeTags(req.body.tags);
+      return superGet(req, userCriteria, options, mainCallback);
+    };
 
-      snippet = { 
-        title: title, 
-        published: published,
-        type: type._instance,
-        approved: false,
-        tags: tags, 
-        areas: {}, 
-        slug: slug, 
-        createdAt: new Date(), 
-        publishedAt: new Date()
-      };
+    self._app.all(self._action + '/' + manager._instance + '/submit', function(req, res) {
+      if (!req.user) {
+        return res.send({ 'status': 'notfound' });
+      }
 
-      snippet.sortTitle = type._apos.sortify(snippet.title);
-      type.convertAllFields('form', req.body, snippet);
+      // Allows use of addFields, removeFields, etc. Otherwise the
+      // user can edit everything which does not make much sense
 
-      var newSnippet = snippet;
+      // In no case should the public be able to pre-publish their work
+      // or set a slug. I'm tempted to ban tags, but that could be useful,
+      // so I leave it up to the developer to remove it if they want to
+      options.removeFields = (options.removeFields || []).concat([ 'published', 'slug' ]);
+      var subsetFields = self._schemas.refine(manager.schema, options);
 
-      type.getOne(req, {}, {}, function(err, result){
-        delete result._id;
-        delete result.publishedAt;
-        delete result.published;
-        delete result.searchSummary;
-        delete result.lowSearchText;
-        delete result.highSearchText;
-        delete result.slug;
+      var piece = manager.newInstance();
 
-        for(field in result) {
-          if(req.body[field] && !snippet[field]) {
-            snippet[field] = req.body[field]
+      if (req.method === 'POST') {
+        self._schemas.convertFields(subsetFields, 'form', req.body, piece);
+        piece.slug = self._apos.slugify(piece.title);
+        piece.submission = true;
+        return async.series({
+          put: function(callback) {
+            return manager.putOne(req, piece.slug, {}, piece, callback);
           }
-        }
-
-        async.series([ permissions, beforeInsert, beforeSave, insert, afterInsert, afterSave ], function(){
-          res.send('/')              
+        }, function(err) {
+          res.send({ status: err ? 'error' : 'ok' });
         });
-
-        function permissions(callback) {
-          type._apos.permissions(req, 'edit-' + type._css, null, callback);
-        }
-
-        function beforeInsert(callback) {
-          type.authorAsEditor(req, snippet);
-          return type.beforeInsert(req, req.body, snippet, callback);
-        }
-
-        function beforeSave(callback) {
-          return type.beforeSave(req, req.body, snippet, callback);
-        }
-
-        function insert(callback) {
-          return type.putOne(req, slug, snippet, callback);
-        }
-
-        function afterInsert(callback) {
-          return type.afterInsert(req, req.body, snippet, callback);
-        }
-
-        function afterSave(callback) {
-          return type.afterSave(req, req.body, snippet, callback);
-        }
-      });
+      } else {
+        return res.send({ status: 'ok', piece: piece, fields: subsetFields, template: manager.render('submissionEditor', { fields: subsetFields }) });
+      }
     });
-  }
-  type.addExtraRoutes();
-  callback();
- }
+  };
+
+  _.each(options.types, function(options, type) {
+    self.enhance(self._pages.getManager(type), options);
+  });
+
+  self.pushAsset('script', 'editor', { when: 'user' });
+  // Construct our browser side object REFACTOR
+  var browserOptions = options.browser || {};
+
+  // The option can't be .constructor because that has a special meaning
+  // in a javascript object (not the one you'd expect, either) http://stackoverflow.com/questions/4012998/what-it-the-significance-of-the-javascript-constructor-property
+  var browser = {
+    construct: browserOptions.construct || 'AposModerator'
+  };
+
+  self._apos.pushGlobalCallWhen('user', 'window.aposModerator = new @(?)', browser.construct, { action: self._action });
+  self.serveAssets();
 
   return process.nextTick(function() {
     return callback(null);
   });
-}
+};
+
